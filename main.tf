f7e69bb29fa8737f9c70d8de373b1360df04fdbe
EC2 Default User <ec2-user@devgit.ap-south-1.compute.internal>
2023-12-13 10:56:02 +0000

    Resource 7 SG for prometheus

--- main.tf
+++ main.tf
@@ -127,3 +127,34 @@ resource "aws_eip" "frontend" {
     owner   = var.project_own
   }
 }
+
+##RESOURCE SECURITY GROUP - PROMETHEOUS ##
+
+resource "aws_security_group" "monitor_access" {
+  name        = "monitor-${var.project_name}-${var.project_env}"
+  description = "${var.project_name}-${var.project_env}"
+
+  ingress {
+    from_port = 9090
+    to_port   = 9090
+    protocol  = "tcp"
+
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  tags = {
+    Name    = "${var.project_name}-${var.project_env}-monitor-access"
+    project = var.project_name
+    env     = var.project_env
+    owner   = var.project_own
+  }
+}
